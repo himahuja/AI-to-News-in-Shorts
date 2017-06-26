360706903e3651077ccbde52753832cd462e27bf
babahooja <babahooja@gmail.com>
2017-06-27 02:22:13 +0530

    Added Time Interval functionality

--- clientnews.js
+++ clientnews.js
@@ -1,27 +1,66 @@
 /////// CREATING VARAIBLE FOR AJAX /////////////////////
 var xmlhttp;
+var time;
+var counter; //pseudo variable
+/*
+The iterator varibale allow different functionality on the same keydown event
+*/
 ////////////// CREATING REQUEST OBJECT ////////////////
 function GetXmlHttpObject(){
   if(window.XMLHttpRequest){
     return new XMLHttpRequest();
   }
   else if(window.ActiveXObject){
-    return new ActiveXObject("Microsoft.XMLHTTP")
+    return new ActiveXObject("Microsoft.XMLHTTP");
   }
   else{
-    return null
+    return null;
   }
 }
 ////////////////////// stateChange ///////////////////////////
-
 function stateChange() {
   if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-    document.getElementById("container").innerHTML = xmlhttp.responseText;
+    document.body.innerHTML = xmlhttp.responseText;
   }
 }
+/////////////////////////////////////////////////////////////////////
+//////////////////////TIME RECODRING HELPERS/////////////////////////
+function timeReset(){
+  time = 0;
+}
+function timeAdd(){
+  time = time + 1;
+}
+////////////////////////////////////////////////////////////////////
+////////////////////// LOADING THE 1st ARTICLE /////////////////////
+////////////////////////////////////////////////////////////////////
+function loadfirstarticle(){
 
-///////////////// change news function /////////////////
+  xmlhttp = new GetXmlHttpObject;
+  if(xmlhttp == null){
+    alert("Your browser does not support XMLHTTP!");
+    return;
+  }
+  var article = "getnews.php";
+  var iid = 0
+  var dir = 0
+  article = article + "?dir=" + dir;
+  article = article + "&iid=" + iid;
+  article = article + "&t=0";
+  xmlhttp.onreadystatechange = stateChange;
+  xmlhttp.open("GET", article, true);
+  xmlhttp.send(null);
+  //Start the time counter after the first article is loaded
+  timeReset();
+  clearInterval(counter);
+  counter = setInterval(timeAdd, 2000);
+}
+
+////////////////////////////////////////////////////////
+///////////////// Change News Function /////////////////
+///////////////////////////////////////////////////////
 document.onkeydown = function(e) {
+  //////////CREATING VARIABLES //////////////////
   e = e || window.event;
   xmlhttp = new GetXmlHttpObject;
   if(xmlhttp == null){
@@ -31,14 +70,21 @@ document.onkeydown = function(e) {
 
   var article = "getnews.php";
   article = article + "?dir=" + e.keyCode;
-  var iid = document.getElementById("identity").innerHTML;
+  var iid = document.getElementById("identity");
   if (iid == null){
     iid = 0;
   }
+  else{
+    iid = iid.innerHTML;
+  }
   article = article + "&iid=" + iid;
+  article = article + "&t=" + time;
   article = article + "&sid=" + Math.random();
   xmlhttp.onreadystatechange = stateChange;
   xmlhttp.open("GET", article, true);
   xmlhttp.send(null);
+  timeReset();
+  clearInterval(counter);
+  counter = setInterval(timeAdd, 2000);
 }
 /////////////////////////////////////////////////
--- getnews.php
+++ getnews.php
@@ -1,5 +1,4 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -12,11 +11,18 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-/////////// GETTING PARAMETERS TO DISPLAY THE NEXT ARTICLE ////////////
+/////////// GETTING PARAMETERS ////////////
 
 $_dir = $_GET["dir"];
 $_iid = $_GET["iid"];
+$_t   = $_GET["t"];
+////////////STORING THE TIME OF THE CURRENT Article/////////////////
+$update = "INSERT INTO stats (iid, bookmark, readmore, counter)
+           VALUES ('$_iid', NULL, NULL, '$_t')
+           ON DUPLICATE KEY UPDATE
+           counter = counter + $_t";
 
+mysqli_query($conn, $update);
 ////////////// SETTING MOTION /////////////
 if($_dir == 38 || $_dir == 37){
   $_dir = -1;
@@ -48,18 +54,19 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)){
-        echo "<section><article id = 'news-card'>
-        <div id = 'identity' style='display:none'>".$row["iid"]."</div>
+        echo "<div id='container'><section><article id = 'news-card'>
+        <div id = 'identity' >".$row["iid"]."</div>
         <h1>". $row["headline"]."</h1>
         <p>".$row["author"]."</p>
         <p>".$row["articleBody"]."</p>
         <a href='#' class='readmore'>Read more</a>
-        <form><input type = 'checkbox'>Bookmark?</input></form>
-        </article></section>";
+        <form action = 'getnews.php' method = 'post'>
+        <input type = 'checkbox' name = 'bookmark'>Bookmark?</input>
+        </form>
+        </article></section></div>";
     }
 } else {
     echo "0 results";
 }
-
 mysqli_close($conn);
 ?>
