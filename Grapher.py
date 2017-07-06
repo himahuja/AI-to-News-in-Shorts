35ea880c68322b327cff132f9b766c3ebde3063a
babahooja <babahooja@gmail.com>
2017-07-07 01:27:03 +0530

    Refined the Grapher Module
    Major Method: Category Grapher

--- Grapher.py
+++ Grapher.py
@@ -1,8 +1,3 @@
-#### GRAPH MODULE
-## TAKES IN THE ENTITIES, each node can have infinite number of edges.
-# The edge will represent how many times two ENTITIES have occured together.
-
-#for forming the graph network
 import networkx as nx
 #for vizualization of the graph
 import matplotlib.pyplot as plt
@@ -11,13 +6,13 @@ import pickle as pk
 #For generating combinations between each pair of nodes
 import itertools
 
-def grapher(nodes):
+def grapher(G, nodes):
+    """
+    INPUT: List of nodes [a, b, c, d], Location of Grapher Pickle file
+    OUTPUT: networkx Graph
+    DEPENDENCY: itertools (for combinations), pickle
+    """
     #Step 1: Load the Graph
-    try:
-        G = pk.load(open('data/categories/entertainmentG.p', 'rb'))
-    except:
-        G = nx.Graph()
-
     if len(nodes)>1:
         #Step 2: Add weight to each of the nodes.
         for node in nodes:
@@ -33,29 +28,64 @@ def grapher(nodes):
                 G[n1][n2]['weight'] += 1
             else:
                 G.add_edge(n1, n2, weight = 1)
-
-    #Step 4: Save and return the graph
-    pk.dump(G, open('data/categories/entertainmentG.p', 'wb'))
+    #Step 4: return the graph
     return G
 
-#nodes_list = pk.load(open('data/categories/entertainment2.p', 'rb'))
-# for nodes in nodes_list:
-#     G = grapher(nodes)
-G = grapher([])
-Gc = max(nx.connected_component_subgraphs(G), key=len)
-pos=nx.shell_layout(Gc) # positions for all nodes
-# nodes
-nx.draw_networkx_nodes(Gc, pos, node_color='gray', node_size=[v * 50 for v in nx.get_node_attributes(Gc,'relevance').values()])
-# edges
-nx.draw_networkx_edges(Gc, pos, edgelist = Gc.edges(), width=1)
-# nx.draw_networkx_edges(G,pos,edgelist=esmall,
-#                     width=6,alpha=0.5,edge_color='b',style='dashed')
-# labels
-nx.draw_networkx_labels(Gc, pos, font_size=10, font_family='sans-serif')
+def plotter(G):
+    """
+    INPUT: Graph (networkx)
+    PROCESS: Gets the layout
+             Gets the nodes
+             Draws edges, labels nodes
+    OUTPUT: matplotlib plot
+    DEPENDENCY: matplotlib.pyplot
+    """
+    pos=nx.spring_layout(G) # positions for all nodes
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_color='gray', \
+    node_size=[v * 50 for v in nx.get_node_attributes(G,'relevance').values()])
+    # edges
+    nx.draw_networkx_edges(G, pos, edgelist = G.edges(), width=1)
+    # labels
+    nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
+    plt.axis('off')
+    # plt.savefig("weighted_graph.png") # save as png
+    plt.show() # display
 
-plt.axis('off')
-# plt.savefig("weighted_graph.png") # save as png
-plt.show() # display
+def category_grapher(fileloc, output):
+    """
+    INPUT : Pickle file of the form list of lists (fileloc), Output Location
+    DEPENDENCY: grapher method
+    OUTPUT: Graph (netwokx), PICKLE file of Graph type
+    """
+    try:
+        G = pk.load(open(output, 'rb'))
+    except:
+        G = nx.Graph()
+        nodes_list = pk.load(open(fileloc, 'rb'))
+        for nodes in nodes_list:
+            G = grapher(G, nodes)
+        G = graph_filter(G)
+        pk.dump(G, open(output, 'wb'))
+    return G
 
-print (G.edges(data=True))
-print (G.nodes(data=True))
+def graph_filter(G):
+    """
+    INPUT: Graph (networkx)
+    PROCESS: Deletes the edges with <= 1 weight,
+             Gets the Largest connected component
+    OUTPUT: Graph (networkx)
+    """
+    weight_dict = nx.get_edge_attributes(G, 'weight')
+    {G.remove_edge(*k) for (k,v) in weight_dict.items() if v <= 1}
+    Gc = max(nx.connected_component_subgraphs(G), key=len)
+    return Gc
+
+def graph_printer(G, databool = False):
+    """
+    Prints the edges and nodes of networkx Graph
+    """
+    print (G.edges(data=databool))
+    print (G.nodes(data=databool))
+
+# category_grapher('data/categories/entertainment2.p')
